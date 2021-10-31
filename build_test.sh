fbd2efef1bd7afbacf10fce1d273075ec6f430eb
1997LP <1950517655@qq.com>
2021-10-31 19:42:19 +0800

    debug

--- build_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-########################################################################
-# File Name: build_test.sh
-# Author: Baiy
-# mail: baiyang0223@163.com
-# Created Time: 2021-04-07-14:14:21
-# Func: 
-#########################################################################
-gcc s_util.c s_test.c   s_bitmap.c s_kfifo.c -o test
